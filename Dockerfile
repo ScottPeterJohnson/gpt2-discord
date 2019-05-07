09e694c2845a986fc0f7eacf054d63631fa653b7
scott <scott@justmachinery.net>
2019-05-06 21:46:32 -0700

    Much simplify interaction, fix multiple newlines in prompt

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM gpt-2-submodule
 RUN apt-get update && apt-get install -y default-jdk
 COPY ./build/install /app
+COPY ./bot_samples.py /gpt-2/src
 WORKDIR /app/gpt2
 CMD ["bin/gpt2"]
--- src/main/kotlin/com/purelymail/gpt2/Main.kt
+++ src/main/kotlin/com/purelymail/gpt2/Main.kt
@@ -6,73 +6,53 @@ import net.dv8tion.jda.core.events.message.MessageReceivedEvent
 import net.dv8tion.jda.core.hooks.ListenerAdapter
 import java.io.ByteArrayOutputStream
 import java.io.File
-import java.io.OutputStream
-import java.util.concurrent.LinkedBlockingDeque
-import kotlin.concurrent.thread
+import java.util.*
 
 fun main(){
     MainServer()
 }
 
 class MainServer {
-    val processInput = startProcess()
+    val process = startProcess()
     val discord = connectDiscord()
 
-    fun startProcess() : OutputStream {
+    fun startProcess() : Process {
         val process = ProcessBuilder()
             .directory(File("/gpt-2"))
-            .command("python3", "src/interactive_conditional_samples.py", "--top_k", "40", "--length", "200", "--model_name", "345M")
+            .command("python3", "src/bot_samples.py", "--top_k", "40", "--length", "200", "--model_name", "345M")
             .redirectError(ProcessBuilder.Redirect.INHERIT)
             .start()
 
-        thread {
-            val buffer = ByteArrayOutputStream()
-            while(true){
-                val result = process.inputStream.read()
-                if(result > 0){
-                    val c = result.toChar()
-                    if(c == '\n'){
-                        outputLines.add(buffer.toByteArray().toString(Charsets.UTF_8))
-                        buffer.reset()
-                    } else {
-                        buffer.write(result)
-                    }
-                } else {
-                    break
-                }
-            }
-        }
-        return process.outputStream
+        return process
     }
 
-    val outputLines = LinkedBlockingDeque<String>()
+    private fun readResponse() : String {
+        val buffer = ByteArrayOutputStream()
+        while(true){
+            val result = process.inputStream.read()
+            if(result > 0){
+                val c = result.toChar()
+                if(c == '\n'){
+                    return buffer.toByteArray().toString(Charsets.UTF_8)
+                } else {
+                    buffer.write(result)
+                }
+            } else {
+                break
+            }
+        }
+        throw java.lang.IllegalStateException("Process terminated")
+    }
 
     fun processRequest(prompt : String) : String {
         synchronized(this){
-            val response = mutableListOf<String>()
-            processInput.write((prompt.trim() + " \n").toByteArray())
-            processInput.flush()
-            var ended = false
-            while(true){
-                val rawLine = outputLines.takeFirst()
-                println("Got line: $rawLine")
-                val line = rawLine.replace("Model prompt >>>", "").replace("======================================== SAMPLE 1 ========================================", "")
-                if(line.isEmpty()){
-                    continue
-                }
-                if(line.trim() == "================================================================================"){
-                    break
-                }
-                if(ended) {
-                    continue
-                }
-                val parts = line.split("<|endoftext|>")
-                if(parts.size > 1){
-                    ended = true
-                }
-                response.add(parts[0])
-            }
-            return response.joinToString("\n")
+            process.outputStream.write(Base64.getEncoder().encode(prompt.toByteArray()))
+            process.outputStream.write("\n".toByteArray())
+            process.outputStream.flush()
+
+            val decoded = Base64.getDecoder().decode(readResponse()).toString(Charsets.UTF_8)
+            val parts = decoded.split("<|endoftext|>")
+            return parts[0]
         }
     }
 
